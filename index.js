56ee5552bf6ed68cf2f57b83547b37568b132e86
Dunnieoo <oluwadunnie@gmail.com>
2022-06-23 10:53:46 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+let firstName = "Oluwaseyi";
+let lastName = "Ogunfunminiyi";
+
+console.log(firstName);
+console.log(lastName);
+
+let height = "5ft 7 inches";
+
+console.log(height);
+
+let country = "Nigeria";
+
+console.log(country);
